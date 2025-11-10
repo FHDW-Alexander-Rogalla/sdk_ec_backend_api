e72ce4e964f582635962fe7cacdc433ebfa88365
FHDW-Alexander-Rogalla <alexander.rogalla@edu.fhdw.de>
2025-11-10 20:47:37 +0200

    Update AssemblyInfo to reflect new informational version and cache hash

--- Sdk_EC_Backend/obj/Debug/net9.0/Sdk_EC_Backend.AssemblyInfo.cs
+++ Sdk_EC_Backend/obj/Debug/net9.0/Sdk_EC_Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sdk_EC_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfdce1591ae0ded9158d605029b3a33d6d083fba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d749a2754bdd7a7d8e4df2978de154ea3cf532ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sdk_EC_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sdk_EC_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
