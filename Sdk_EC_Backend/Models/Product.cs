3531fa3d1a8216566ba2df34055d2ab3403a9589
FHDW-Alexander-Rogalla <alexander.rogalla@edu.fhdw.de>
2025-11-10 22:13:07 +0200

    Refactor Product class to improve code formatting and readability

--- Sdk_EC_Backend/Models/Product.cs
+++ Sdk_EC_Backend/Models/Product.cs
@@ -8,12 +8,24 @@ public class Product : BaseModel
 {
     [PrimaryKey("id", false)]
     public long Id { get; set; }
+    
+    [Column("name")]
     public string Name { get; set; } = string.Empty;
+
+    [Column("description")]
     public string? Description { get; set; }
+
+    [Column("price")]
     public decimal Price { get; set; }
+
+    [Column("image_url")]
     public string? ImageUrl { get; set; }
+
     // public int StockQuantity { get; set; }
     // public string? Category { get; set; }
+    [Column("created_at")]
     public DateTime CreatedAt { get; set; }
+
+    [Column("updated_at")]
     public DateTime UpdatedAt { get; set; }
 }
